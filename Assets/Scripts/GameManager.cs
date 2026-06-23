0d6883afb43a0067dc3d41746b0b49e99a727b6e
Joel <joelbalto04@gmail.com>
2026-06-22 23:05:37 -0700

    Created card prefab and started game manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private NumberCard cardPrefab;
+    void Start()
+    {
+        SpawnStartingCards();
+    }
+
+    public void SpawnStartingCards()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/NumberCard.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+public class NumberCard : MonoBehaviour
+{
+    public int value;
+    private string expression;
+    [SerializeField] private TMP_Text topLeftText;
+    [SerializeField] private TMP_Text bottomRightText;
+    void Start()
+    {
+        // maybe do something
+    }
+
+    void SetValue(int val)
+    {
+        value = val;
+        // this will most likely be changed
+        expression = value.ToString();
+        topLeftText.SetText(" " + value.ToString());
+        bottomRightText.SetText(" " + value.ToString());
+    }
+}
